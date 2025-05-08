779c6601f994175a746c054772c1a0e076c73ae0
matpin <stamkouk92@gmail.com>
2025-05-08 12:22:03 +0300

    Add ESLint configuration & setup

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['**/node_modules', '**/dist'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslint,
+    },
+    rules: {
+      ...tsEslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  prettierConfig,
+];
