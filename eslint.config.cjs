7d4bfcc4b2beef4f6fa3bb461e71f8a9f2ea1923
matpin <stamkouk92@gmail.com>
2025-05-08 12:45:00 +0300

    Fix ESLint error

---
+++ eslint.config.cjs
@@ -0,0 +1,26 @@
+const tsEslint = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
+const prettierConfig = require("eslint-config-prettier");
+
+module.exports = [
+  {
+    ignores: ["**/node_modules", "**/dist"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsEslint,
+    },
+    rules: {
+      ...tsEslint.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  prettierConfig,
+];
